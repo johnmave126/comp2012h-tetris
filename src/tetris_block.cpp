bc1b3cee5eab3d8505b8db5e5fc95a2938e13aaa
JohnMave126 <johnmave126@gmail.com>
2013-03-24 05:12:23 +0800

    debug
    Signed-off-by: JohnMave126 <johnmave126@gmail.com>

--- src/tetris_block.cpp
+++ src/tetris_block.cpp
@@ -18,8 +18,10 @@
 #include <qfile.h>
 #include <qdir.h>
 
+#include <iostream>
 #include <cstdlib>
 #include <cassert>
+using namespace std;
 
 TetrisBlock::TetrisBlock(int type, int direction, QObject* parent, const char* name)
 :QObject(parent, name),
@@ -261,11 +263,13 @@ int TetrisBlock::putBoard(const QColor grid[][BOARD_HEIGHT + 1]) const {
 				//Solid block
 				//Border check
 				if(baseX + i < 0 || baseX + i >= GRID_WIDTH
-					|| baseY + j < 0 || baseY + j >= GRID_HEIGHT) {
-						return 1;
+				 || baseY + j < 0 || baseY + j >= GRID_HEIGHT) {
+					cout << "Border!" << endl;
+					return 1;
 				}
 				//Collision check
 				if(grid[i][j].rgb() != zero) {
+					cout << "Collision!" << endl;
 					return 1;
 				}
 			}
